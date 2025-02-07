38c4e41686cb32cd1a1f6e6e12cb2e7dc6991a33
Edwin Krivoseev <edwin.krivoseev@fullhaus.de>
2025-02-07 10:19:20 +0100

    fix: add missing react configs

--- configs/react.mjs
+++ configs/react.mjs
@@ -1,7 +1,18 @@
 import rules from '../rules/react.js';
+import react from 'eslint-plugin-react';
 
 export default [
   {
+    plugins: {
+      react,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
     rules: rules,
   },
 ];
