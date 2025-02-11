11ea981c4def0f2041dcbe58e4c0f1768e8c74e2
Edwin Krivoseev <edwin.krivoseev@fullhaus.de>
2025-02-11 12:35:00 +0100

    feat: provide improved type annotation with description

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,81 @@
-// Suppress missing declaration warnings
+import { Linter } from 'eslint';
+
 declare module 'eslint-config-fullhaus';
 
 declare const _default: {
+  /**
+   * Collection of configurations that set up all required settings and plugins out of the box
+   */
   configs: {
-    default: Array<Record<string, any>>;
-    base: Array<Record<string, any>>;
-    typescript: Array<Record<string, any>>;
-    react: Array<Record<string, any>>;
+    /**
+     * Default configuration which combines these configurations:
+     * - {@linkcode baseConfig}
+     * - {@linkcode commentsConfig}
+     * - {@linkcode stylisticConfig}
+     * - {@linkcode typescriptConfig}
+     */
+    default: Linter.Config[];
+    /**
+     * JavaScript configuration which extends Eslint's recommended rules.
+     */
+    baseConfig: Linter.Config[];
+    /**
+     * Configuration for enforced descriptive comments for eslint and TypeScript disables/ignores.
+     */
+    commentsConfig: Linter.Config[];
+    /**
+     * Configuration for react (.jsx).
+     */
+    reactConfig: Linter.Config[];
+    /**
+     * Configuration for enforced code style.
+     */
+    stylisticConfig: Linter.Config[];
+    /**
+     * Configuration for react while using TypeScript (.tsx).
+     */
+    typedReactConfig: Linter.Config[];
+    /**
+     * Configuration for TypeScript
+     */
+    typescriptConfig: Linter.Config[];
   }
+  /**
+   * Collection of rules used for configuration
+   */
+  rules: {
+    /**
+     * Rules for basic ESLint
+     * @see https://eslint.org/docs/latest/rules/
+     */
+    baseRules: Linter.RulesRecord,
+    /**
+     * Rules for ESLint comments
+     * @see https://eslint-community.github.io/eslint-plugin-eslint-comments/
+     */
+    commentsRules: Linter.RulesRecord,
+    /**
+     * Rules for ESLint react
+     * @see https://github.com/jsx-eslint/eslint-plugin-react?tab=readme-ov-file#list-of-supported-rules
+     */
+    reactRules: Linter.RulesRecord,
+    /**
+     * Rules for ESLint Stylistic
+     * @see https://eslint.style/rules
+     */
+    stylisticRules: Linter.RulesRecord,
+    /**
+     * TypeScript (`.tsx`) adjusted rules for ESLint react
+     * @see https://github.com/jsx-eslint/eslint-plugin-react?tab=readme-ov-file#list-of-supported-rules
+     * @see https://typescript-eslint.io/rules/
+     */
+    typedReactRules: Linter.RulesRecord,
+    /**
+     * Rules for typescript-eslint
+     * @see https://typescript-eslint.io/rules/
+     */
+    typescriptRules: Linter.RulesRecord,
+  };
 }
 
 export default _default;
